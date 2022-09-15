5d8b9716da1bae36d58dfb8885083995bb136a95
Nikolay Krishtopa <nickey87@yandex.ru>
2022-09-15 09:56:00 +0300

    hotfix/reduce height of content to show footer

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const API_KEY = process.env.REACT_APP_API_KEY
 
 const config = {
   apiData: {
-    baseUrl: `http://www.omdbapi.com/?apikey=${API_KEY}`,
+    baseUrl: `https://www.omdbapi.com/?apikey=${API_KEY}`,
     headers: {
       'Content-Type': 'application/json',
       referrer: 'http://www.omdbapi.com/',
